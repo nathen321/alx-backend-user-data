af8bc82678bb70df940c1642450f2b1ae8082b14
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-05-28 00:49:27 +0100

    Add BasicAuth class for basic authentication handling

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""  manage the API authentication
+"""
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """...
+
+    Args:
+        Auth (type): description
+    """
+    pass
