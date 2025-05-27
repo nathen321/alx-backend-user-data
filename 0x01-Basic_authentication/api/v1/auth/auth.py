1491cf56a7d0e2c6673e83b7fb8c17ac62aaac18
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-05-27 11:48:12 +0100

    Refactor Auth class docstrings for clarity and consistency

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,36 +2,20 @@
 """  manage the API authentication
 """
 from flask import request
+from typing import List, TypeVar
 
 
 class Auth():
-    """ the API authentication
-    """
+    """ Class to manage the API authentication """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """_summary_
-
-        Args:
-            path (str): _description_
-            excluded_paths (List[str]): _description_
-
-        Returns:
-            bool: _description_
-        """
+        """ Method for validating if endpoint requires auth """
         return False
 
     def authorization_header(self, request=None) -> str:
-        """_summary_
-
-        Args:
-            request (_type_, optional): _description_. Defaults to None.
-
-        Returns:
-            str: _description_
-        """
+        """ Method that handles authorization header """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """_summary_
-        """
+        """ Validates current user """
         return None
