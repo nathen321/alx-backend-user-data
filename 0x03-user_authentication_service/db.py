284cf784dc3b1c13dba035f3a60729d3a103c781
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-06-09 19:12:58 +0100

    Implement DB module with SQLAlchemy for user management

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -42,4 +42,5 @@ class DB:
         session = self._session
         new_user = User(email=email, hashed_password=hashed_password)
         session.add(new_user)
+        session.commit()
         return new_user
