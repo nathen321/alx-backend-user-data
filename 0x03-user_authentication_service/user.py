cfae309fe29f1527c5165ade4fc7dd0930e69a4f
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-06-09 17:25:13 +0100

    Refactor User model in user.py for consistency and clarity

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,20 +2,19 @@
 """
 Users model
 """
-from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
-
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class User (Base):
+class User(Base):
     """ Represents user object/table
     """
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=False)
-    reset_token = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
