acc0bbfb67ccc068cb8f2bab5173a7fc3ce5f5b5
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-06-09 23:41:14 +0100

    Implement Auth class with user registration functionality

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,25 @@
 Hash password
 """
 import bcrypt
+from db import DB
+from user import User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        user = self._db.find_user_by(email=email)
+        if user:
+            raise ValueError(f"User ${user.email} already exists")
+        else:
+            hash = _hash_password(password)
+            return self._db.add_user(
+                email=email, hashed_password=hash.decode('utf-8'))
 
 
 def _hash_password(password: str) -> bytes:
