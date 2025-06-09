b572ea6283a29e76e6b73009bb2c22f76c4fef75
walid jalaly <46631425+nathen321@users.noreply.github.com>
2025-06-09 23:23:13 +0100

    Implement password hashing function using bcrypt

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Hash password
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
